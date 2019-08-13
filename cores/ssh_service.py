4acb45f7b1dcfeada11fc15b8f19498f45d85608
nguyenvanhoang7398 <nguyenvanhoang7398@gmail.com>
2019-08-13 16:21:56 +0800

    Handle ssh exceptions

--- cores/ssh_service.py
+++ cores/ssh_service.py
@@ -7,9 +7,13 @@ class SshService(object):
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def execute(self, host, command):
-        self.client.connect(host)  # assume that ssh connection is password-less
-        stdout = self.client.exec_command(command, get_pty=True)[1]
-        return [str(m) for m in stdout]
+        try:
+            self.client.connect(host)  # assume that ssh connection is password-less
+            stdout = self.client.exec_command(command, get_pty=True)[1]
+            return [str(m) for m in stdout]
+        except paramiko.SSHException as e:
+            print(e)
+        return []
 
     def close(self):
         self.client.close()
